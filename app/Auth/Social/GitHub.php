d17778740468de2511efba9c4ca7c470eb3f94f9
Evgeniy <wizard2014@ya.ru>
2017-03-19 13:17:21 +0200

    Add GitHub auth

--- app/Auth/Social/GitHub.php
+++ app/Auth/Social/GitHub.php
@@ -6,11 +6,52 @@ class GitHub extends Service
 {
     public function getAuthorizeUrl()
     {
-
+        return 'https://github.com/login/oauth/authorize?client_id=CLIENT_ID&redirect_uri=http://REDIRECT_URI&scope=user,user:email&state=RENDOM_STR';
     }
 
     public function getUserByCode($code)
     {
-        
+        $token = $this->getAccessTokenFromCode($code);
+
+        return $this->normalizeUser($this->getUserByToken($token));
+    }
+
+    protected function getUserByToken($token)
+    {
+        $response = $this->client->request('GET', 'https://api.github.com/user', [
+            'query' => [
+                'access_token' => $token
+            ]
+        ])->getBody();
+
+        return json_decode($response);
+    }
+
+    protected function getAccessTokenFromCode($code)
+    {
+        $response = $this->client->request('GET', 'https://github.com/login/oauth/access_token', [
+                'query' => [
+                    'client_id' => 'CLIENT_ID',
+                    'client_secret' => 'CLIENT_SECRET',
+                    'redirect_uri' => 'REDIRECT_URI',
+                    'code' => $code,
+                    'state' => 'RENDOM_STR',
+                ],
+                'headers' => [
+                    'accept' => 'application/json',
+                ]
+            ])->getBody();
+
+        return json_decode($response)->access_token;
+    }
+
+    protected function normalizeUser($user)
+    {
+        return (object) [
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'photo' => $user->avatar_url,
+        ];
     }
 }
--- index.php
+++ index.php
@@ -4,4 +4,11 @@ require 'app/bootstrap.php';
 
 $auth = new \App\Auth\Social\GitHub($client);
 
-$auth->authorizeUrl();
+if (!isset($_GET['code'])) {
+    echo 'Ivalid code';
+    exit;
+}
+
+$user = $auth->getUser($_GET['code']);
+
+var_dump($user);
